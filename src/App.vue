0232eb4cc1ec44026ba665c83a044c313b33a36b
winning\wu_qiang <wu_qiang@winning.com.cn>
2022-08-05 16:49:05 +0800

    安装基本包

--- src/App.vue
+++ src/App.vue
@@ -5,4 +5,14 @@
 </template>
 
 <style>
+html,
+body,
+#app {
+  height: 100%;
+  width: 100%;
+}
+* {
+  padding: 0;
+  margin: 0;
+}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,16 @@
 <template>
-  <div class="home">11111</div>
+  <div class="home">
+    <el-container>
+      <el-header>Header</el-header>
+      <el-container>
+        <el-aside width="200px">Aside</el-aside>
+        <el-container>
+          <el-main>Main</el-main>
+          <el-footer>Footer</el-footer>
+        </el-container>
+      </el-container>
+    </el-container>
+  </div>
 </template>
 
 <script>
@@ -8,3 +19,10 @@ export default {
   components: {},
 }
 </script>
+<style scoped  lang="scss">
+.home,
+.el-container {
+  height: 100%;
+  width: 100%;
+}
+</style>
